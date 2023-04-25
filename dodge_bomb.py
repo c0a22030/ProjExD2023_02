45f4653ddf66c80b975d710b0f4d369041a76b30
ryoya <c0a2203012@edu.teu.ac.jp>
2023-04-25 14:32:13 +0900

    練習問題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -15,6 +15,9 @@ def main():
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 中心に半径10の赤い円
     bb_img.set_colorkey((0, 0, 0))  # 黒を透過
     x, y = random.randint(0, 1600), random.randint(0, 900)  # 爆弾のx座標とｙ座標をスクリーンの範囲でランダムに指定
+    vx, vy = +1, +1  # 爆弾の速度を指定
+    bb_rect = bb_img.get_rect()  # rectクラスをインプット
+    bb_rect.center = x, y  # 爆弾の座標を指定
 
     while True:
         for event in pg.event.get():
@@ -24,7 +27,8 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [x,y])  # 爆弾を表示
+        bb_rect.move_ip(vx, vy)  # 爆弾の移動
+        screen.blit(bb_img, bb_rect)  # 爆弾を表示
 
         pg.display.update()
         clock.tick(1000)
