bdbec834a5df8da434090adf08d0e5488851842e
ryoya <c0a2203012@edu.teu.ac.jp>
2023-04-25 14:19:49 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,7 @@
-import pygame as pg
+import random
 import sys
+
+import pygame as pg
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -9,9 +11,10 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
-    bb_img = pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_img.set_colorkey((0, 0, 0))
+    bb_img = pg.Surface((20, 20))  # 1辺が20の正方形
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 中心に半径10の赤い円
+    bb_img.set_colorkey((0, 0, 0))  # 黒を透過
+    x, y = random.randint(0, 1600), random.randint(0, 900)  # 爆弾のx座標とｙ座標をスクリーンの範囲でランダムに指定
 
     while True:
         for event in pg.event.get():
@@ -21,6 +24,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, [x,y])  # 爆弾を表示
 
         pg.display.update()
         clock.tick(1000)
